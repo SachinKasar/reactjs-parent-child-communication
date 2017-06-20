c7cc31284ee5c1c4141d96e9e99ddf0ef6102552
SachinKasar <kasarsac@WL308554.ap.tieto.com>
2017-06-20 12:38:55 +0530

    Upadates for README and files renaming

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # reactjs-parent-child-communication
-reactjs-parent-child-communication
+
+Component communication
+
+1. Communication by state mutation
+2. Passing state as props 
+
+
+
+**Steps to follow**
+
+:one: clone git repo with 
+**npm clone https://github.com/SachinKasar/reactjs-parent-child-communication.git**
+
+:two: install npm packages with command
+**npm install**
+
+:three: start webpack server
+**npm start**
+
+:four: hit the url in browser **http://localhost:8080/**
+
+
+
---
+++ src/app/components/TwoWheeler.js
@@ -0,0 +1,59 @@
+import React from "react";
+import {render} from "react-dom";
+
+// TwoWheel as a Class Component
+export class TwoWheeler extends React.Component {
+	constructor(props) {
+		super();
+	    //this.price = props.price;
+		this.state = {price:props.price, visitor:'Visitor 2'}
+		this.handleOnClick = this.handleOnClick.bind(this);
+		this.handleOnClickVisitor = this.handleOnClickVisitor.bind(this);
+	}
+	
+	handleOnClick() {
+		// this.price = "Rs.100000";
+		this.setState({price:'Rs. 100000'});
+		console.log('New Price');
+	}
+	
+	handleOnClickVisitor() {
+		// this.price = "Rs.100000";
+		this.props.changeVisitor(this.state.visitor);
+		console.log('New Visitor');
+	}
+	
+    render() {
+        return ( 
+		    <div className="container">
+                <div className="row">
+				    <div className="row">
+						<h1 style={{color:'blue'}}> Two Wheeler </h1>
+						<ul>
+							<li>
+								<b>Name :</b> {this.props.name}
+							</li>
+							<li>
+								<b>By :</b> {this.props.specs.manufacturer}
+							</li>
+							
+							<li>
+								<b>Price :</b> {this.state.price}
+							</li>
+							<li>
+							<b>Visitor :</b>  {this.props.visitor}
+							</li>
+							
+						</ul>
+						<div className="btn-group btn-group-sm">
+							<button onClick={this.handleOnClick} className="btn btn-primary" >Get New Price</button>
+							<button onClick={this.props.greet} className="btn btn-primary" >Greet Customer</button>
+							<button onClick={this.handleOnClickVisitor} className="btn btn-primary" >Recent Visitor</button>
+						</div>
+					</div>
+				</div>
+				</div>
+      
+        );
+    } 
+}
